ae8722ccd0dbc98e28281cbb8e576b9c6827a146
Marcha <marchaf@gmail.com>
2022-10-20 10:28:39 +0200

    update existing proj before ts update

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,28 +1,12 @@
-import React, { Component } from "react";
+import Card from '../card/card.component';
+import './card-list.styles.css';
 
-import { Card } from "../card/card.component";
-
-import "./card-list.styles.css";
-
-// export const CardList = (props) => (
-//   <div className="card-list">
-//     {props.monsters.map((monster) => (
-//       <Card key={monster.id} monster={monster} />
-//     ))}
-//   </div>
-// );
-
-export class CardList extends Component {
-  render() {
-    const { monsters } = this.props;
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => (
-          <Card key={monster.id} monster={monster} />
-        ))}
-      </div>
-    );
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
